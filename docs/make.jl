ec72a3f62e42ce9468e87d6746c0af49b3ff9a29
Loan Vulliard <lvulliard@cemm.oeaw.ac.at>
2021-03-01 19:48:53 +0100

    Add examples and structure docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,9 @@
 using Documenter, RMP
 
-makedocs(sitename="RMP.jl")
+makedocs(sitename="RMP.jl",
+		 pages = [
+	        "RMP.jl" => "index.md",
+	        "API" => "api.md",
+	        "Examples" => "examples.md"
+         ],
+         authors = "Loan Vulliard" )
